8e0862027129ff41acd565327f7a1eb803f82399
Amir Dezyanian <30830017+famirco@users.noreply.github.com>
2024-10-30 22:05:25 +0330

    init

--- app/controllers/api/v1/price_records_controller.rb
+++ app/controllers/api/v1/price_records_controller.rb
@@ -2,23 +2,8 @@ module Api
   module V1
     class PriceRecordsController < ApplicationController
       def index
-        @prices = PriceRecord.includes(:token).order(created_at: :desc)
-        render json: @prices
-      end
-
-      def create
-        @price = PriceRecord.new(price_record_params)
-        if @price.save
-          render json: @price, status: :created
-        else
-          render json: @price.errors, status: :unprocessable_entity
-        end
-      end
-
-      private
-
-      def price_record_params
-        params.require(:price_record).permit(:token_id, :price_usdc, :gas_fee, :rpc_url)
+        @price_records = PriceRecord.includes(:token).order(created_at: :desc).limit(100)
+        render json: @price_records
       end
     end
   end
--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -5,11 +5,6 @@ module Api
         @tokens = Token.all
         render json: @tokens
       end
-
-      def show
-        @token = Token.find(params[:id])
-        render json: @token
-      end
     end
   end
 end
