8a4a70a072d7cb739a0002c4ce45a0e4160102d0
Amir Dezyanian <30830017+famirco@users.noreply.github.com>
2024-10-30 21:53:40 +0330

    init

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'  # در محیط production باید محدودتر شود
+      origins 'https://amirez.info'
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
