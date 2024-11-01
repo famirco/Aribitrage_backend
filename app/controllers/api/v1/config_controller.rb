9bafe212fcb52d1117221c42b1fc0e325d8a759d
Amir Dezyanian <30830017+famirco@users.noreply.github.com>
2024-11-01 10:31:56 +0330

    init

--- app/controllers/api/v1/config_controller.rb
+++ app/controllers/api/v1/config_controller.rb
@@ -1,14 +1,14 @@
 module Api
-    module V1
-      class ConfigController < ApplicationController
-        def index
-          config = {
-            tokens: Setting.tokens,
-            networks: Setting.networks
-          }
-          
-          render json: config
-        end
+  module V1
+    class ConfigController < ApplicationController
+      def index
+        config = {
+          tokens: Setting.tokens,
+          networks: Setting.networks
+        }
+        
+        render json: config
       end
     end
   end
+end
