2c295ae017a69f19d3abd14d20fd21e5db8737aa
Amir Dezyanian <30830017+famirco@users.noreply.github.com>
2024-11-01 10:28:43 +0330

    init

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :tokens, only: [:index]
       resources :price_records, only: [:index]
       resources :arbitrage_opportunities, only: [:index]
+      resources :config, only: [:index]
     end
   end
 end
